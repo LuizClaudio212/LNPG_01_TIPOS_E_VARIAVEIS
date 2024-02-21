892b1e2fb30f2a4d3004374686432bc634a3a00d
Luiz Claudio <138629461+LuizClaudio212@users.noreply.github.com>
2024-02-21 18:03:40 -0300

    Add files via upload

---
+++ Atividades em Haskell/Codigos em Haskell referente a atividade/Par_ou_Impar.hs
@@ -0,0 +1,10 @@
+main = do
+  putStrLn "Verificador de Par ou Impar"
+  putStrLn ""
+  putStrLn "Digite um numero: "
+  numero <- readLn :: IO Int
+  putStrLn ""
+  if even numero 
+        then putStrLn "O número é par"
+        else putStrLn "O número é impar"
+  
